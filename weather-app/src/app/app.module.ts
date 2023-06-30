7f19a2faecd6b63d985e4552eb454f710428d816
djanr2 <djanr2@hotmail.com>
2023-06-30 00:12:59 -0600

    20230630  new workspace for weather-app for angular 12

--- weather-app/src/app/app.module.ts
+++ weather-app/src/app/app.module.ts
@@ -1,25 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
 import { AppComponent } from './app.component';
-import { HttpClientModule } from "@angular/common/http";
-import { WeatherComponent } from './weather/weather.component';
-import {MatIconModule} from "@angular/material/icon";
-import {MatInputModule} from "@angular/material/input";
-import {MatCardModule} from "@angular/material/card";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    WeatherComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    MatIconModule,
-    MatInputModule,
-    MatCardModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
