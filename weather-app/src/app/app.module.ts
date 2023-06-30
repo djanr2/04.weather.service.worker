ebf94a60efa8cc6bd7dff4731b85d926b66eba38
djanr2 <djanr2@hotmail.com>
2023-06-29 22:55:44 -0600

    20230629 Se inicia la aplicacion de weather para obtener informacion de un api, Se agregan alguson estilos de angular/material

--- weather-app/src/app/app.module.ts
+++ weather-app/src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import { AppComponent } from './app.component';
+import { HttpClientModule } from "@angular/common/http";
+import { WeatherComponent } from './weather/weather.component';
+import {MatIconModule} from "@angular/material/icon";
+import {MatInputModule} from "@angular/material/input";
+import {MatCardModule} from "@angular/material/card";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatIconModule,
+    MatInputModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ weather-app/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://api.openweathermap.org/data/2.5/',
+  apiKey: 'ce32bf8a9fd2ff65d6c450ad5996e0d4'
+};
